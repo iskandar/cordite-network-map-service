c95df577328e5dfb3422d16e95c8cd3a3acce0ad
Fuzz <fuzz@bluebank.io>
2018-04-10 15:36:26 +0100

    fix naming issue

--- az-deploy.sh
+++ az-deploy.sh
@@ -9,7 +9,7 @@ echo "registery password:"
 read -s PASSWORD
 
 az login
-az container delete --resource-group-name cordite-network-map --name cordite-network-map
+az container delete --resource-group cordite-network-map --name cordite-network-map
 az container create --resource-group cordite-network-map --name cordite-network-map --image ${IMAGE} --ports ${PORT} --registry-login-server registry.gitlab.com --registry-username ${USERNAME} --registry-password ${PASSWORD} --dns-name-label cordite-network-map
 echo 'running: az container show --resource-group cordite-network-map --name cordite-network-map --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table'
 az container show --resource-group cordite-network-map --name cordite-network-map --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table
