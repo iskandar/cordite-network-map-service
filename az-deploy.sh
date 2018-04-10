ebb551cb891ea413e749627830e2069433efa012
Fuzz <fuzz@bluebank.io>
2018-04-10 15:32:33 +0100

    environment variable for port, set to 80

--- az-deploy.sh
+++ az-deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/sh
 
 IMAGE=registry.gitlab.com/cordite/network-map-service/network-map-service:latest
+PORT=80
 echo "logging into gitlab docker registery"
 echo "registery username:"
-read -r username
+read -r USERNAME
 echo "registery password:"
-read -s password
+read -s PASSWORD
 
 az login
 az container delete --name cordite-network-map
-az container create --resource-group cordite-network-map --name cordite-network-map --image ${IMAGE} --ports 8080 --registry-login-server registry.gitlab.com --registry-username ${username} --registry-password ${password} --dns-name-label cordite-network-map
+az container create --resource-group cordite-network-map --name cordite-network-map --image ${IMAGE} --ports ${PORT} --registry-login-server registry.gitlab.com --registry-username ${USERNAME} --registry-password ${PASSWORD} --dns-name-label cordite-network-map
 echo 'running: az container show --resource-group cordite-network-map --name cordite-network-map --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table'
 az container show --resource-group cordite-network-map --name cordite-network-map --query "{FQDN:ipAddress.fqdn,ProvisioningState:provisioningState}" --out table
 
--- src/main/kotlin/io/cordite/services/NetworkMapApp.kt
+++ src/main/kotlin/io/cordite/services/NetworkMapApp.kt
@@ -5,16 +5,24 @@ import io.vertx.core.Future
 import io.vertx.core.Vertx
 import io.vertx.ext.web.Router
 
-class NetworkMapApp(val host: String, val port: Int) : AbstractVerticle() {
+class NetworkMapApp(private val port: Int) : AbstractVerticle() {
   companion object {
     val logger = loggerFor(NetworkMapApp::class)
     const val WEB_ROOT = "/network-map"
 
     @JvmStatic
     fun main(args: Array<String>) {
-      val port = (System.getProperty("port") ?: "8080").toInt()
+      val port = getPort()
       val host = (System.getProperty("host") ?: "localhost")
-      NetworkMapApp(host, port).deploy()
+      NetworkMapApp(port).deploy()
+    }
+
+    private fun getPort() : Int {
+      return getVariable("port", "8080").toInt()
+    }
+
+    private fun getVariable(name: String, default: String) : String {
+      return (System.getenv(name) ?: System.getProperty(name) ?: default)
     }
   }
 
@@ -23,14 +31,14 @@ class NetworkMapApp(val host: String, val port: Int) : AbstractVerticle() {
   }
 
   override fun start(startFuture: Future<Void>) {
-    logger.info("starting network map with host: $host port: $port")
+    logger.info("starting network map with port: $port")
 
     val router = Router.router(vertx)
     router.get("/").handler {
-      it.end("hello")
+      it.end("hello, v3")
     }
     router.get(WEB_ROOT).handler {
-      it.end("hello")
+      it.end("hello, v3")
     }
 
     vertx
@@ -41,7 +49,7 @@ class NetworkMapApp(val host: String, val port: Int) : AbstractVerticle() {
             logger.error("failed to startup", it.cause())
             startFuture.fail(it.cause())
           } else {
-            logger.info("networkmap service started on http://$host:$port$WEB_ROOT")
+            logger.info("networkmap service started on http://localhost:$port$WEB_ROOT")
             startFuture.complete()
           }
         }
