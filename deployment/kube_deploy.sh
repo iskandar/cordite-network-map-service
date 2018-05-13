d5f660c307d6ce61c6bf9af0513ffd4860e31abf
Richard Crook <richard.crook@bluebank.io>
2018-05-13 19:30:38 +0100

    edge

--- deployment/kube_deploy.sh
+++ deployment/kube_deploy.sh
@@ -1,10 +1,11 @@
 #!/bin/sh
 # deploy app to kube
-# usage ./kube_deploy.sh  <image tag>
+# usage ./kube_deploy.sh
 
-IMAGE_TAG=${1:-latest}
+IMAGE_TAG=${CI_PIPELINE_ID:-latest}
 KUBE_NAMESPACE=${KUBE_NAMESPACE:-default}
 GITLAB_USER_EMAIL=${GITLAB_USER_EMAIL:-nobody@example.com}
+CI_ENVIRONMENT_URL=${CI_ENVIRONMENT_URL:-network-map-dev.cordite.biz}
 
 echo "$(date) deploying app to namespace ${KUBE_NAMESPACE} with image ${IMAGE_TAG}"
 
@@ -40,7 +41,11 @@ kubectl create secret -n "$KUBE_NAMESPACE" \
     -o yaml --dry-run | kubectl replace -n "$KUBE_NAMESPACE" --force -f -
 
 # Replace deployment
-cat ./deployment.yaml | sed s/:latest/:${IMAGE_TAG}/ | kubectl create -n "$KUBE_NAMESPACE" -o yaml --dry-run -f - | kubectl replace -n "$KUBE_NAMESPACE" --force -f -
+cat ./deployment.yaml \
+ | sed s/:latest/:${IMAGE_TAG}/ \
+ | sed s/network-map-dev.cordite.biz/${CI_ENVIRONMENT_URL}/ \
+ | kubectl create -n "$KUBE_NAMESPACE" -o yaml --dry-run -f - \
+ | kubectl replace -n "$KUBE_NAMESPACE" --force -f -
 
 # once we are done show everything again
 kubectl get all -n "$KUBE_NAMESPACE"
