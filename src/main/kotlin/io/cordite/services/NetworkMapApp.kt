26b145437d346de87a6bb3ff9a367b42628683d3
Fuzz <fuzz@bluebank.io>
2018-05-21 22:54:41 +0100

    separate out command-line parsing from instantiation

--- src/main/kotlin/io/cordite/services/NetworkMapApp.kt
+++ src/main/kotlin/io/cordite/services/NetworkMapApp.kt
@@ -2,15 +2,16 @@ package io.cordite.services
 
 import io.cordite.services.utils.Options
 import io.cordite.services.utils.toFile
-import io.vertx.core.AbstractVerticle
 import io.vertx.core.Vertx
 import net.corda.core.utilities.loggerFor
 import java.io.File
 import java.time.Duration
 
-open class NetworkMapApp(private val port: Int,
-                         private val dbDirectory: File
-) : AbstractVerticle() {
+open class NetworkMapApp(
+  private val dbDirectory: File,
+  private val port: Int,
+  private val cacheTimeout: Duration
+)  {
   companion object {
     val logger = loggerFor<NetworkMapApp>()
 
@@ -27,9 +28,13 @@ open class NetworkMapApp(private val port: Int,
       val port = portOption.value.toInt()
       val db = dbDirectory.value.toFile()
       val ct = Duration.parse(cacheTimeout.value)
-      val vertx = Vertx.vertx()
-      val service = NetworkMapService(dbDirectory = db, port = port, cacheTimeout = ct)
-      vertx.deployVerticle(service)
+      NetworkMapApp(db, port, ct).start()
     }
+  }
+
+  private fun start() {
+    val vertx = Vertx.vertx()
+    val service = NetworkMapService(dbDirectory = dbDirectory, port = port, cacheTimeout = cacheTimeout)
+    vertx.deployVerticle(service)
   }
 }
