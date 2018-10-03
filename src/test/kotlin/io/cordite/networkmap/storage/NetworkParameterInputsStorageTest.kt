d45dd2d863e1aaac5ec3b258b3c851b52e9b0cff
ajithathayahar <ajitha.thayaharan@bcstechnology.com.au>
2018-10-03 16:55:21 +1000

    added test cases

--- src/test/kotlin/io/cordite/networkmap/storage/NetworkParameterInputsStorageTest.kt
+++ src/test/kotlin/io/cordite/networkmap/storage/NetworkParameterInputsStorageTest.kt
@@ -151,23 +151,53 @@ class NetworkParameterInputsStorageTest {
   }
 
 
-  /*@Test
+  @Test
   fun `that we can upload validating notary nodeInfo`(context: TestContext) {
-    TODO("Yet to work on the test case")
+    val tempDir = createTempDirectory()
+    val nmis = NetworkParameterInputsStorage(tempDir, vertx)
+    val nodeInfoPath= File("${SAMPLE_INPUTS}validating/", "nodeInfo-007A0CAE8EECC5C9BE40337C8303F39D34592AA481F3153B0E16524BAD467533")
+    nmis.makeDirs()
+      .onSuccess { println("directories created in ${nmis.directory}") }
+      .compose {
+        vertx.fileSystem().readFile(nodeInfoPath.absolutePath).map {
+          nmis.postValidatingNotaryNodeInfo(it)
+        }
+      }
+      .onSuccess {
+        NetworkParameterInputsStorage.log.info("uploaded validating notary nodeInfo")
+        System.out.println("${nmis.validatingNotariesPath.absolutePath}/nodeInfo-007A0CAE8EECC5C9BE40337C8303F39D34592AA481F3153B0E16524BAD467533")
+        vertx.fileSystem().exists("${nmis.validatingNotariesPath.absolutePath}/nodeInfo-007A0CAE8EECC5C9BE40337C8303F39D34592AA481F3153B0E16524BAD467533",{
+          context.assertEquals(true, it.result())
+        })
+      }
+      .catch {
+        context.fail(it)
+      }
+      .setHandler(context.asyncAssertSuccess())
   }
 
   @Test
   fun `that we can upload non-validating notary nodeInfo`(context: TestContext) {
-    TODO("Yet to work on the test case")
+    val tempDir = createTempDirectory()
+    val nmis = NetworkParameterInputsStorage(tempDir, vertx)
+    val nodeInfoPath= File("${SAMPLE_INPUTS}non-validating/", "nodeInfo-B5CD5B0AD037FD930549D9F3D562AB9B0E94DAB8284DB205E2E82F639EAB4341")
+    nmis.makeDirs()
+      .onSuccess { println("directories created in ${nmis.directory}") }
+      .compose {
+        vertx.fileSystem().readFile(nodeInfoPath.absolutePath).map {
+          nmis.postNonValidatingNotaryNodeInfo(it)
+        }
+      }
+      .onSuccess {
+        NetworkParameterInputsStorage.log.info("uploaded non validating notary nodeInfo")
+        System.out.println("${nmis.nonValidatingNotariesPath.absolutePath}/nodeInfo-B5CD5B0AD037FD930549D9F3D562AB9B0E94DAB8284DB205E2E82F639EAB4341")
+        vertx.fileSystem().exists("${nmis.nonValidatingNotariesPath.absolutePath}/nodeInfo-B5CD5B0AD037FD930549D9F3D562AB9B0E94DAB8284DB205E2E82F639EAB4341",{
+          context.assertEquals(true, it.result())
+        })
+      }
+      .catch {
+        context.fail(it)
+      }
+      .setHandler(context.asyncAssertSuccess())
   }
-
-  @Test
-  fun `that we cant upload multiple notary nodeInfo`(context: TestContext) {
-    TODO("Yet to work on the test case")
-  }
-
-  @Test
-  fun `that we cant upload invalid notary nodeInfo`(context: TestContext) {
-    TODO("Yet to work on the test case")
-  }*/
 }
