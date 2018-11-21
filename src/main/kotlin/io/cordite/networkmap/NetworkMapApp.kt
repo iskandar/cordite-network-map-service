4ea216dc753a16103ef55029d2edd84d170a17fb
Fuzz <git@dazraf.io>
2018-11-21 00:11:04 +0000

    a first cut to show how mongod can be started and stopped from the NMS

--- src/main/kotlin/io/cordite/networkmap/NetworkMapApp.kt
+++ src/main/kotlin/io/cordite/networkmap/NetworkMapApp.kt
@@ -15,12 +15,28 @@
  */
 package io.cordite.networkmap
 
+import com.mongodb.async.SingleResultCallback
+import com.mongodb.async.client.MongoCollection
+import de.flapdoodle.embed.mongo.MongodStarter
+import de.flapdoodle.embed.mongo.config.MongoCmdOptionsBuilder
+import de.flapdoodle.embed.mongo.config.MongodConfigBuilder
+import de.flapdoodle.embed.mongo.config.Net
+import de.flapdoodle.embed.mongo.config.Storage
+import de.flapdoodle.embed.mongo.distribution.Version
+import de.flapdoodle.embed.process.runtime.Network
 import io.cordite.networkmap.service.CertificateManagerConfig
 import io.cordite.networkmap.service.NetworkMapService
 import io.cordite.networkmap.utils.LogInitialiser
 import io.cordite.networkmap.utils.NMSOptions
+import io.cordite.networkmap.utils.onSuccess
+import io.vertx.core.Future
 import net.corda.core.utilities.loggerFor
+import org.litote.kmongo.async.KMongo
+import org.litote.kmongo.async.ensureIndex
+import org.litote.kmongo.async.getCollection
+import java.io.File
 import kotlin.system.exitProcess
+
 
 open class NetworkMapApp {
   companion object {
@@ -36,7 +52,78 @@ open class NetworkMapApp {
         }
         println("starting networkmap with the following options")
         printOptions()
+        connectToDatabase()
         bootstrapNMS()
+      }
+    }
+
+    data class Foo(val bar: String)
+
+    private fun NMSOptions.connectToDatabase() {
+      val starter = MongodStarter.getDefaultInstance()
+
+      val bindIp = "localhost"
+      val port = Network.getFreeServerPort()
+      val location = File(this.dbDirectory, "mongo")
+      location.mkdirs()
+
+      val replication = Storage(location.absolutePath, null, 0)
+      val mongodConfig = MongodConfigBuilder()
+        .version(Version.Main.PRODUCTION)
+        .net(Net(bindIp, port, Network.localhostIsIPv6()))
+        .replication(replication)
+        .cmdOptions(MongoCmdOptionsBuilder()
+          .syncDelay(10)
+          .useNoPrealloc(false)
+          .useSmallFiles(false)
+          .useNoJournal(false)
+          .enableTextSearch(true)
+          .build())
+        .build()
+
+      try {
+        starter.prepare(mongodConfig).let { mongodExecutable ->
+          mongodExecutable.start()
+          val mongo = KMongo.createClient("mongodb://$bindIp:$port")
+          val db = mongo.getDatabase("test")
+          val col = db.getCollection<Foo>()
+          col.ensureIndex(Foo::bar)
+          col.insertOne(Foo("bar"))
+            .compose { col.count() }
+            .onSuccess {
+              println("inserted one item. count is $it")
+            }
+          Runtime.getRuntime().addShutdownHook(Thread {
+            mongodExecutable.stop()
+          })
+        }
+      } catch (err: Throwable) {
+        logger.error("failed to startup mongod", err)
+      }
+    }
+
+    private fun <T> MongoCollection<T>.insertOne(value: T): Future<Unit> {
+      val result = Future.future<Void>()
+      this.insertOne(value, singleCallback(result))
+      return result.mapEmpty()
+    }
+
+    private fun <T> MongoCollection<T>.count(): Future<Long> {
+      val result = Future.future<Long>()
+      this.count(singleCallback(result))
+      return result
+    }
+
+    private fun <T> singleCallback(future: Future<T>): SingleResultCallback<T> {
+      return SingleResultCallback { value, err ->
+        future.completeFrom(value, err)
+      }
+    }
+
+    private fun <T> Future<T>.completeFrom(value: T?, err: Throwable?) {
+      return when {
+        err != null -> fail(err)
+        else -> complete(value)
       }
     }
 
